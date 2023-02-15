d3910c5ec45dadb90fd0e3a5159d786286d0d36e
Jhonatan Carvalho <jhonatan.silva.4198@gmail.com>
2023-02-14 22:13:47 -0300

    Update App.js - Now the game show who gonna play next and who is the winner

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function Board() {
 
   function handleClick(i) {
     //check if square already have value
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -27,13 +27,21 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-
-    setSquares(nextSquares);
     setXIsNext(!xIsNext);
+    setSquares(nextSquares);
+  }
+
+  let status;
+  const winner = calculateWinner(squares);
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next Player: ${xIsNext ? "X" : "O"}`;
   }
 
   return (
     <>
+      <div className="status"> {status} </div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -51,4 +59,26 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      console.info("winner!");
+      return squares[a];
+    }
+  }
+  return null;
 }
